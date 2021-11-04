a07f26fdd55917511ede85c0e7dd8fb60e3cc0cd
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2021-11-04 16:59:39 +1300

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+// module.exports = {
+//   mongoURI = 'mongodb+srv://myFlixDB-admin:Admin123@aya-cluster.stqui.mongodb.net/nzWineDB?retryWrites=true&w=majority'
+// }
---
+++ routes/api/items.js
@@ -0,0 +1,30 @@
+// const express = require('express');
+// const router = express.Router;
+
+// // Models
+// const Models = require('../../models/models');
+// const Wines = Models.Wine;
+// const Regions = Models.Region;
+
+// // @route GET api/regions
+// // @desc Return a list of all nz wine regions
+// // @access Public
+
+
+// // @route GET api/regions/[name]
+// // @desc Return data about an nz wine region by name
+// // @access Public
+
+
+// // @route GET api/varieties
+// // @desc Return a list of all nz wine varieties
+// // @access Public
+
+// // @route GET api/varieties/[name]
+// // @desc Return data about an nz wine variety by name
+// // @access Public
+
+
+
+
+// module.exports = router;
