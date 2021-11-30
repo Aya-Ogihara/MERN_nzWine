f6121413d0873e2b5a4008a22c48757349b5c62b
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2021-11-30 14:48:39 +1300

    Setup for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path')
 
 //const items = require('./routes/api/items');
 
@@ -22,10 +23,21 @@ mongoose
 // Use Routes  
 //app.use('/api/items', items)
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 // Cors
 app.use(cors({
   origin: 'http://localhost:3000'
 }))
+
 // Models
 const Models = require('./models');
 const Wines = Models.Wine;
